8d17f5117f277389b8b43d65ec46cad672c811f7
okstring <milkoi7801@gmail.com>
2025-04-09 17:49:32 +0900

    test: big, medium, small button 테스트 위젯 작성

---
+++ test/presentation/components/f_small_button_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:recipe_app/presentation/components/f_small_button.dart';
+
+void main() {
+  group('FSmallButton 테스트', () {
+    testWidgets('FSmaillButton text should match', (WidgetTester tester) async {
+      const text = 'Hello';
+      voidCallback() {}
+
+      await tester.pumpWidget(MaterialApp(
+          home: FSmallButton(text: text, voidCallback: voidCallback)
+      ));
+
+      final Finder textFinder = find.text(text);
+
+      expect(textFinder, findsOneWidget);
+    });
+
+    testWidgets('FSmaillButton should trigger onTap event when clicked', (WidgetTester tester) async {
+      int count = 0;
+      const text = 'Hello';
+      voidCallback() {
+        count++;
+      }
+
+      await tester.pumpWidget(MaterialApp(
+          home: FSmallButton(text: text, voidCallback: voidCallback)
+      ));
+
+      final Finder textFinder = find.text(text);
+      await tester.tap(textFinder);
+
+      expect(count, 1);
+    });
+  });
+}
