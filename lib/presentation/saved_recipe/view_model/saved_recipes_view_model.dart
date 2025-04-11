0604b3c46c352c72171c847062fb0869930eb5af
okstring <milkoi7801@gmail.com>
2025-04-11 13:37:10 +0900

    feat: saved recipe view model 구현

---
+++ lib/presentation/saved_recipe/view_model/saved_recipes_view_model.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:recipe_app/data/model/recipe.dart';
+import 'package:recipe_app/data/repository/recipe_repository.dart';
+
+class SavedRecipesViewModel with ChangeNotifier {
+  final RecipeRepository _recipeRepository;
+
+  final List<Recipe> _savedRecipes = [];
+
+  List<Recipe> get savedRecipes => List.unmodifiable(_savedRecipes);
+
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  SavedRecipesViewModel({
+    required RecipeRepository recipeRepository,
+  }) : _recipeRepository = recipeRepository;
+
+  void fetchSavedRecipes() async {
+    _isLoading = true;
+    notifyListeners();
+
+    final savedRecipes = await _recipeRepository.getSavedRecipe();
+    _savedRecipes.addAll(savedRecipes);
+    _isLoading = false;
+    notifyListeners();
+  }
+}
