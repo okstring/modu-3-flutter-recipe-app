8c882e69fd8cd8870297c67fb0b829dcce89a613
okstring <milkoi7801@gmail.com>
2025-04-14 13:20:09 +0900

    feat: inputField 위젯 수정 (돋보기 visible 조건 추가)

--- lib/presentation/components/f_input_field.dart
+++ lib/presentation/components/f_input_field.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:recipe_app/ui/color_styles.dart';
 import 'package:recipe_app/ui/text_styles.dart';
 
-
 class FInputField extends StatelessWidget {
   final String label;
   final String placeHolder;
   final String value;
   final Key _textFieldKey;
+  final bool isVisibleSearchIcon;
   final void Function(String) onValueChange;
 
   const FInputField({
@@ -15,6 +15,7 @@ class FInputField extends StatelessWidget {
     required this.label,
     required this.placeHolder,
     required this.value,
+    required this.isVisibleSearchIcon,
     required this.onValueChange,
     required Key textFieldKey,
   }): _textFieldKey = textFieldKey;
@@ -23,41 +24,60 @@ class FInputField extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      height: 81,
+      height: 83,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        spacing: 5,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            child: Text(label, style: TextStyles.smallTextRegular(color: AppColors.black),),
-          ),
+          if (label.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5),
+              child: Text(
+                label,
+                style: TextStyles.smallTextRegular(color: AppColors.black),
+              ),
+            ),
 
           Container(
             width: double.infinity,
-            height: 55,
-            alignment: Alignment.centerLeft,
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: placeHolder,
-                hintStyle: TextStyles.smallerTextRegular(color: AppColors.gray4),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(color: AppColors.gray4, width: 1.5),
+            height: 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: AppColors.gray4, width: 1.5),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                isVisibleSearchIcon
+                  ? Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Icon(Icons.search, color: AppColors.gray4, size: 18),
+                  )
+                : const SizedBox.shrink(),
+
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: placeHolder,
+                      hintStyle: TextStyles.smallerTextRegular(color: AppColors.gray4),
+                      border: InputBorder.none,
+                      contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
+                      alignLabelWithHint: true,
+                      isDense: true,
+                    ),
+                    style: TextStyles.smallerTextRegular(color: AppColors.black),
+                    key: _textFieldKey,
+                    controller: TextEditingController(text: value),
+                    onChanged: onValueChange,
+                    expands: false,
+                  ),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(color: AppColors.primary80, width: 1.5),
-                ),
-                contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
-              ),
-              style: TextStyles.smallerTextRegular(color: AppColors.black),
-              key: _textFieldKey,
-            )
-            // Container(
-            //   child: Text(placeHolder, style: TextStyles.smallerTextRegular(color: AppColors.gray4),),
-            // ),
-          )
+
+                const SizedBox(width: 10),
+              ],
+            ),
+          ),
         ],
       ),
     );
