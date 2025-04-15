5fde03429e7d97fd141a93f5fa7a34be84ac6930
okstring <milkoi7801@gmail.com>
2025-04-16 01:09:55 +0900

    add: assets 추가, 탭 별로 빈 screen 구현

---
+++ lib/presentation/notifications/notifications_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NotificationsScreen extends StatelessWidget {
+  const NotificationsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          '알람 페이지',
+        ),
+      ),
+    );
+  }
+}
